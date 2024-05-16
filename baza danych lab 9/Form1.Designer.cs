4925bd5b4e9f16f48c58a656eea88d44608fe73b
juliaordecka <juliaordecka@gmail.com>
2024-05-16 04:18:45 +0200

    dziala poprawnie

--- baza danych lab 9/Form1.Designer.cs
+++ baza danych lab 9/Form1.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             Label label2;
+            Label label5;
+            Label label6;
+            Label label7;
+            Label label8;
+            Label label9;
+            Label label10;
             label1 = new Label();
             textBox1 = new TextBox();
             label3 = new Label();
@@ -36,17 +42,89 @@
             textBox3 = new TextBox();
             button1 = new Button();
             button2 = new Button();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            label11 = new Label();
             label2 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
             SuspendLayout();
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(70, 124);
+            label2.Location = new Point(34, 124);
             label2.Name = "label2";
             label2.Size = new Size(105, 20);
             label2.TabIndex = 2;
             label2.Text = "imieINazwisko";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(20, 160);
+            label5.Name = "label5";
+            label5.Size = new Size(119, 20);
+            label5.TabIndex = 14;
+            label5.Text = "kierunekIStopien";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(170, 296);
+            label6.Name = "label6";
+            label6.Size = new Size(78, 20);
+            label6.TabIndex = 15;
+            label6.Text = "przedmiot";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(443, 293);
+            label7.Name = "label7";
+            label7.Size = new Size(53, 20);
+            label7.TabIndex = 16;
+            label7.Text = "punkty";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(170, 336);
+            label8.Name = "label8";
+            label8.Size = new Size(89, 20);
+            label8.TabIndex = 18;
+            label8.Text = "prowadzacy";
+            label8.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(173, 410);
+            label9.Name = "label9";
+            label9.Size = new Size(95, 20);
+            label9.TabIndex = 19;
+            label9.Text = "uzasadnienie";
+            label9.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(48, 201);
+            label10.Name = "label10";
+            label10.Size = new Size(89, 20);
+            label10.TabIndex = 21;
+            label10.Text = "semestrIRok";
+            label10.TextAlign = ContentAlignment.TopCenter;
             // 
             // label1
             // 
@@ -63,11 +141,12 @@
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(125, 27);
             textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(70, 88);
+            label3.Location = new Point(34, 88);
             label3.Name = "label3";
             label3.Size = new Size(103, 20);
             label3.TabIndex = 3;
@@ -75,17 +154,19 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(176, 88);
+            textBox2.Location = new Point(143, 85);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(125, 27);
             textBox2.TabIndex = 4;
+            textBox2.TextChanged += textBox2_TextChanged;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(176, 124);
+            textBox3.Location = new Point(143, 124);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(125, 27);
             textBox3.TabIndex = 5;
+            textBox3.TextChanged += textBox3_TextChanged;
             // 
             // button1
             // 
@@ -95,6 +176,7 @@
             button1.TabIndex = 6;
             button1.Text = "zapis";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
@@ -104,12 +186,103 @@
             button2.TabIndex = 7;
             button2.Text = "odczyt";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(62, 17);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 8;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(34, 20);
+            label4.Name = "label4";
+            label4.Size = new Size(22, 20);
+            label4.TabIndex = 9;
+            label4.Text = "Id";
+            label4.Click += label4_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(145, 157);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 10;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(254, 293);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(125, 27);
+            textBox6.TabIndex = 11;
+            textBox6.TextChanged += textBox6_TextChanged;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(502, 286);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(125, 27);
+            textBox7.TabIndex = 12;
+            textBox7.TextChanged += textBox7_TextChanged;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(265, 336);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(125, 27);
+            textBox8.TabIndex = 13;
+            textBox8.TextChanged += textBox8_TextChanged;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(274, 410);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(125, 27);
+            textBox9.TabIndex = 17;
+            textBox9.TextChanged += textBox9_TextChanged;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(143, 194);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(125, 27);
+            textBox10.TabIndex = 20;
+            textBox10.TextChanged += textBox10_TextChanged;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(245, 249);
+            label11.Name = "label11";
+            label11.Size = new Size(351, 20);
+            label11.TabIndex = 22;
+            label11.Text = "wniosek o przeprowadzenie egzaminu komisyjnego";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 566);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(textBox10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(textBox9);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(textBox3);
@@ -120,6 +293,7 @@
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -133,5 +307,14 @@
         private TextBox textBox3;
         private Button button1;
         private Button button2;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private Label label11;
     }
 }
--- baza danych lab 9/Form1.cs
+++ baza danych lab 9/Form1.cs
@@ -12,10 +12,22 @@ using Microsoft.Data.SqlClient;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 using Microsoft.Identity.Client;
 
+
 namespace baza_danych_lab_9
 {
     public partial class Form1 : Form
     {
+        public string userdata;
+        public int userid;
+        public string usernumerAlbumu;
+        public string userimieINazwisko;
+        public string userkierunekIStopien;
+        public string userprzedmiot;
+        public string userpunkty;
+        public string userprowadzacy;
+        public string useruzasadnienie;
+        public string usersemestrIRok;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,66 +35,191 @@ namespace baza_danych_lab_9
 
         public class DatabaseManager
         {
-            private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=""C:\USERS\JULIA\SOURCE\REPOS\BAZA DANYCH LAB 9\BAZA DANYCH LAB 9\DATABASE1.MDF"";Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
             public void ReadData()
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    string query = "SELECT * FROM Table";
+                    string query = "SELECT * FROM Lol";
                     SqlCommand command = new SqlCommand(query, connection);
                     try
                     {
                         connection.Open();
                         SqlDataReader reader = command.ExecuteReader();
-                        while (reader.Read())
+
+                        // Check if there are rows returned
+                        if (reader.HasRows)
                         {
-                            int userId = reader.GetInt32(0); // Assuming Id is the first column
-                            string userdata = reader.GetString(1);
-                            string usernumerAlbumu = reader.GetString(2);
-                            string imieINazwisko = reader.GetString(3);
-                            // Assuming Name is the second column
-                            Console.WriteLine($"User Id: {userId}, data: {userdata}");
+                            while (reader.Read())
+                            {
+                                int rId = reader.GetInt32(0);
+                                string rdata = reader.GetValue(1).ToString();
+                                string rnumerAlbumu = reader.GetValue(2).ToString();
+                                string rimieINazwisko = reader.GetValue(3).ToString();
+                                string rkierunekIStopien = reader.GetValue(4).ToString();
+                                string rprzedmiot = reader.GetValue(5).ToString();
+                                string rpunkty = reader.GetValue(6).ToString();
+                                string rprowadzacy = reader.GetValue(7).ToString();
+                                string ruzasadnienie = reader.GetValue(8).ToString();
+                                string rsemestrIRok = reader.GetValue(9).ToString();
+
+                                MessageBox.Show($"User Id: {rId}, data: {rdata}, Numer Albumu: {rnumerAlbumu}, Imie i Nazwisko: {rimieINazwisko}, Kierunek i Stopieñ: {rkierunekIStopien}, Przedmiot: {rprzedmiot}, Punkty: {rpunkty}, Prowadz¹cy: {rprowadzacy}, Uzasadnienie: {ruzasadnienie}, Semestr i Rok: {rsemestrIRok}");
+                            }
                         }
+                        else
+                        {
+                            MessageBox.Show("No data found.");
+                        }
+
                         reader.Close();
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("SQL Error: " + ex.Message);
                     }
                     catch (Exception ex)
                     {
-                        Console.WriteLine("Error: " + ex.Message);
+                        MessageBox.Show("Error: " + ex.Message);
                     }
                 }
             }
 
-            public void WriteData(int userId, string userdata, string usernumerAlbumu, string userimieINazwisko)
+
+
+            public void WriteData(int userId, string userdata, string usernumerAlbumu, string userimieINazwisko, string userkierunekIStopien, string userprzedmiot, string userpunkty, string userprowadzacy, string useruzasadnienie, string usersemestrIRok)
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    string query = "INSERT INTO Table (Id, data, numerAlbumu, imieINazwisko) VALUES (@userId, @userdata, @usernumerAlbumu, @userimieINazwisko)";
+                    string query = "INSERT INTO Lol (Id, data, numerAlbumu, imieINazwisko, kierunekIStopien, przedmiot, punkty, prowadzacy, uzasadnienie, semestrIRok) VALUES (@idd, @dataa, @numeralbumuu, @imieiinazwisko, @kierunekiistopien, @przedmiott, @punktyy, @prowadzacyy, @uzasadnieniee, @semestriirok)";
                     SqlCommand command = new SqlCommand(query, connection);
-                    command.Parameters.AddWithValue("@Id", userId);
-                    command.Parameters.AddWithValue("@data", userdata);
-                    command.Parameters.AddWithValue("@numerAlbumu", usernumerAlbumu);
-                    command.Parameters.AddWithValue("@imieINazwisko", userimieINazwisko);
-                    command.ExecuteNonQuery();
-                    connection.Open();
-                    int rowsAffected = command.ExecuteNonQuery();
-                    Console.WriteLine($"{rowsAffected} row(s) inserted.");
+                    command.Parameters.AddWithValue("@idd", userId);
+                    command.Parameters.AddWithValue("@dataa", userdata);
+                    command.Parameters.AddWithValue("@numeralbumuu", usernumerAlbumu);
+                    command.Parameters.AddWithValue("@imieiinazwisko", userimieINazwisko);
+                    command.Parameters.AddWithValue("@kierunekiistopien", userkierunekIStopien);
+                    command.Parameters.AddWithValue("@przedmiott", userprzedmiot);
+                    command.Parameters.AddWithValue("@punktyy", userpunkty);
+                    command.Parameters.AddWithValue("@prowadzacyy", userprowadzacy);
+                    command.Parameters.AddWithValue("@uzasadnieniee", useruzasadnienie);
+                    command.Parameters.AddWithValue("@semestriirok", usersemestrIRok);
+                    try
+                    {
+                        connection.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
+                        MessageBox.Show($"{rowsAffected} row(s) inserted.");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
                 }
             }
+
         }
 
-        //zapis
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            userdata = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            usernumerAlbumu = textBox2.Text;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            userimieINazwisko = textBox3.Text;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            userid = Convert.ToInt32(textBox4.Text);
+        }
+
+        //zapis button
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
 
-            WriteData(2, "aaa", "bbb", "ccc");
+                DatabaseManager db = new DatabaseManager();
+                db.WriteData(userid, userdata, usernumerAlbumu, userimieINazwisko, userkierunekIStopien, userprzedmiot, userpunkty, userprowadzacy, useruzasadnienie, usersemestrIRok);
+                MessageBox.Show("ok");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+
         }
         //odczyt
         private void button2_Click(object sender, EventArgs e)
         {
+            DatabaseManager db = new DatabaseManager();
+            db.ReadData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=""C:\USERS\JULIA\SOURCE\REPOS\BAZA DANYCH LAB 9\BAZA DANYCH LAB 9\DATABASE1.MDF"";Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        MessageBox.Show("udalo sie polaczyc z baza danych");
+                    }
+                    else
+                    {
+                        MessageBox.Show("nie udalo sie polaczyc z baza danych");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
 
         }
 
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            userkierunekIStopien = textBox5.Text;
+        }
 
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            userprzedmiot = textBox6.Text;
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            userpunkty = textBox7.Text;
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            userprowadzacy = textBox8.Text;
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+            useruzasadnienie = textBox9.Text;
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+            usersemestrIRok = textBox10.Text;
+        }
     }
 }
